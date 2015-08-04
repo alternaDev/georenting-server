f589534c311cb54223d7356a6ac3f45782a1cb9e
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-08-04 20:44:38 +0200

    Added Gorilla Router

--- main.go
+++ main.go
@@ -6,10 +6,28 @@ import (
     "os"
     "log"
     "github.com/alternaDev/geomodel"
+    "github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s! %f", r.URL.Path[1:], geomodel.Distance(0, 0, 1, 1))
+  vars := mux.Vars(r)
+
+  fmt.Fprintf(w, "Hi there, I love %s! %f", vars["name"], geomodel.Distance(0, 0, 1, 1))
+}
+
+func setupRouter() *mux.Router {
+  r := mux.NewRouter()
+
+  r.HandleFunc("/{name}", handler)
+
+  return r
+}
+
+func listen(port string) {
+  r := setupRouter()
+
+  http.Handle("/", r)
+  http.ListenAndServe(":" + port, nil)
 }
 
 func main() {
@@ -19,7 +37,5 @@ func main() {
   		log.Fatal("$PORT must be set")
   	}
 
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":" + port, nil)
-
+    listen(port)
 }
