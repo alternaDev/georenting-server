7f6f3ba1988fbcb510db17457dabe169eb4826dc
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-09-07 23:28:06 +0200

    Implemented redis url parsing

--- models/redis.go
+++ models/redis.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 
 	"gopkg.in/redis.v3"
@@ -9,10 +10,13 @@ import (
 
 var RedisInstance = initRedis(os.Getenv("REDIS_URL"))
 
-func initRedis(url string) *redis.Client {
+func initRedis(www string) *redis.Client {
+	redisUrl, _ := url.Parse(www)
+	password, _ := redisUrl.User.Password()
 	client := redis.NewClient(&redis.Options{
-		Addr: url,
-		DB:   0,
+		Addr:     redisUrl.Host,
+		Password: password,
+		DB:       0,
 	})
 
 	pong, err := client.Ping().Result()
