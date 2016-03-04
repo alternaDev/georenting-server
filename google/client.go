d2809497a91664533e1ad23e0ea35596ee1f4da8
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-03-04 17:31:28 +0100

    Added new Version of Google Checks

--- google/client.go
+++ google/client.go
@@ -4,21 +4,37 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+	"os"
 )
 
 const (
-	googleVerifyURL = "https://www.googleapis.com/plus/v1/people/me"
+	googleVerifyURL  = "https://www.googleapis.com/oauth2/v3/tokeninfo"
+	googleProfileURL = "https://www.googleapis.com/plus/v1/people/me"
 )
+
+// TokenInfoResponse Gets the Token Info from da server.
+type TokenInfoResponse struct {
+	Issuer        string `json:"iss"`
+	Audit         string `json:"aud"`
+	Sub           string `json:"sub"`
+	EmailVerified bool   `json:"email_verified"`
+	Email         string `json:"email"`
+	Name          string `json:"name"`
+	Avatar        string `json:"picture"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Locale        string `json:"locale"`
+}
 
 // User represents the data for a given Google User
 type User struct {
-	GoogleID      string     `json:"id"`
-	Name          string     `json:"displayName"`
-	GooglePlusURL string     `json:"url"`
-	Avatar        Avatar     `json:"image"`
-	Cover         Cover      `json:"cover"`
-	Gender        string     `json:"gender"`
-	Locale        string     `json:"language"`
+	GoogleID      string `json:"id"`
+	Name          string `json:"displayName"`
+	GooglePlusURL string `json:"url"`
+	Avatar        Avatar `json:"image"`
+	Cover         Cover  `json:"cover"`
+	Gender        string `json:"gender"`
+	Locale        string `json:"language"`
 }
 
 // Avatar represents a users avatar
@@ -42,9 +58,10 @@ type CoverPhoto struct {
 func VerifyToken(token string) (User, error) {
 	client := &http.Client{}
 
-	req, err := http.NewRequest("GET", googleVerifyURL, nil)
-	req.Header.Add("Access_token", token)
-	req.Header.Add("Authorization", "OAuth "+token)
+	req, err := http.NewRequest("GET", googleVerifyURL+"?id_token="+token, nil)
+
+	//req.Header.Add("Access_token", token)
+	//req.Header.Add("Authorization", "OAuth "+token)
 
 	if err != nil {
 		return User{}, err
@@ -61,6 +78,37 @@ func VerifyToken(token string) (User, error) {
 	}
 
 	decoder := json.NewDecoder(resp.Body)
+	var response TokenInfoResponse
+	err = decoder.Decode(&response)
+
+	if err != nil {
+		return User{}, err
+	}
+
+	if response.Audit != os.Getenv("GOOGLE_CLIENT_ID") {
+		return User{}, errors.New("Invalid Client.")
+	}
+
+	// TODO: Check whether the Token was valid.
+
+	req, err = http.NewRequest("GET", googleProfileURL, nil)
+	req.Header.Add("Access_token", token)
+	req.Header.Add("Authorization", "OAuth "+token)
+
+	if err != nil {
+		return User{}, err
+	}
+
+	resp, err = client.Do(req)
+
+	if err != nil {
+		return User{}, err
+	}
+
+	if resp.StatusCode != 200 {
+		return User{}, errors.New("Invalid Token")
+	}
+
 	var user User
 	err = decoder.Decode(&user)
 
