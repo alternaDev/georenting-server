93363cf8066b1ca69b2d38097658437215908ded
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-05-17 23:41:41 +0200

    Fixed Overlap algorithm

--- controllers/fences.go
+++ controllers/fences.go
@@ -261,14 +261,22 @@ func checkFenceOverlap(fence *models.Fence) (bool, error) {
 
 	for i := range result {
 		fenceB := result[i]
-		//   D = sqrt( (Ax - Bx)2 + (Ay - By)2 )
-		distance := math.Sqrt((fence.Lat - fenceB.Lat) * (fence.Lat - fenceB.Lat) +
-														(fence.Lon - fenceB.Lon) * (fence.Lon - fenceB.Lon))
+		distance := distance(fence.Lat, fence.Lon, fenceB.Lat, fenceB.Lon)
 		if distance < float64(fence.Radius + fenceB.Radius) {
 			return true, nil
 		}
 	}
 	return false, nil
+}
+
+func distance(lat1 float64, lon1 float64, lat2 float64, lon2 float64) float64 {
+	R := 6378.137;
+	dLat := (lat2 - lat1) * math.Pi / 180
+	dLon := (lon2 - lon1) * math.Pi / 180;
+	a := math.Sin(dLat / 2) * math.Sin(dLat / 2) + math.Cos(lat1 * math.Pi / 180) * math.Cos(lat2 * math.Pi / 180) * math.Sin(dLon / 2) * math.Sin(dLon / 2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := R * c
+	return d * 1000
 }
 
 // GetFenceHandler GET /fences/{fenceId}
