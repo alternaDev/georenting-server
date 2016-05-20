ac38d300226325ac86196214a83abbc59befa15d
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-05-20 12:37:48 +0200

    Added prepare statement for que table

--- jobs/jobs.go
+++ jobs/jobs.go
@@ -5,6 +5,33 @@ import (
 	"github.com/jackc/pgx"
 )
 
+const (
+	QueTableSQL = `
+		CREATE TABLE IF NOT EXISTS que_jobs
+		(
+			priority    smallint    NOT NULL DEFAULT 100,
+			run_at      timestamptz NOT NULL DEFAULT now(),
+			job_id      bigserial   NOT NULL,
+			job_class   text        NOT NULL,
+			args        json        NOT NULL DEFAULT '[]'::json,
+			error_count integer     NOT NULL DEFAULT 0,
+			last_error  text,
+			queue       text        NOT NULL DEFAULT '',
+			CONSTRAINT que_jobs_pkey PRIMARY KEY (queue, priority, run_at, job_id)
+		);` // QueTableSQL to create table idempotently
+)
+
+// prepQue ensures that the que table exists and que's prepared statements are
+// run. It is meant to be used in a pgx.ConnPool's AfterConnect hook.
+func prepQue(conn *pgx.Conn) error {
+	_, err := conn.Exec(QueTableSQL)
+	if err != nil {
+		return err
+	}
+
+	return que.PrepareStatements(conn)
+}
+
 func GetPgxPool(dbURL string) (*pgx.ConnPool, error) {
 	pgxcfg, err := pgx.ParseURI(dbURL)
 	if err != nil {
@@ -13,12 +40,14 @@ func GetPgxPool(dbURL string) (*pgx.ConnPool, error) {
 
 	pgxpool, err := pgx.NewConnPool(pgx.ConnPoolConfig{
 		ConnConfig:   pgxcfg,
-		AfterConnect: que.PrepareStatements,
+		AfterConnect: prepQue,
 	})
 
 	if err != nil {
 		return nil, err
 	}
+
+	defer pgxpool.Close()
 
 	return pgxpool, nil
 }
