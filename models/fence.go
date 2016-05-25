205c7cd917b379753c85ba3b309e6e03910fa82d
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-05-25 21:42:11 +0200

    Added Upgrade Types to Fence Model

--- models/fence.go
+++ models/fence.go
@@ -1,21 +1,25 @@
 package models
 
 import (
+	"github.com/alternaDev/georenting-server/maths"
 	"github.com/jinzhu/gorm"
 )
 
-const (
-	FenceMinRadius = 100
-	FenceMaxRadius = 1000
+var (
+	UpgradeTypesRadius = [...]int{100, 150, 200, 250, 300, 350, 400}
+	UpgradeTypesRent   = [...]float32{1, 1.5, 2, 2.5, 3, 3.5, 4}
+	UpgradeTypesTTL    = [...]int{3, 5, 7}
+	FenceMinRadius     = maths.Min(UpgradeTypesRadius[:])
+	FenceMaxRadius     = maths.Max(UpgradeTypesRadius[:])
 )
 
 // Fence is a fence
 type Fence struct {
 	gorm.Model
-	User     User      `json:"-"`
-	UserID   uint      `json:"ownerId"`
-	Lat      float64   `json:"centerLat"`
-	Lon      float64   `json:"centerLon"`
-	Radius   int       `json:"radius"`
-	Name     string    `json:"name"`
+	User   User    `json:"-"`
+	UserID uint    `json:"ownerId"`
+	Lat    float64 `json:"centerLat"`
+	Lon    float64 `json:"centerLon"`
+	Radius int     `json:"radius"`
+	Name   string  `json:"name"`
 }
