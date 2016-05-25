13f7a5b400f3ab397d3de43d31d0087048e94f24
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-05-25 21:43:47 +0200

    Added Maths Package File

---
+++ maths/extremes.go
@@ -0,0 +1,21 @@
+package maths
+
+func Max(vals []int) int {
+	var max = vals[0]
+	for v := range vals {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func Min(vals []int) int {
+	var min = vals[0]
+	for v := range vals {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
