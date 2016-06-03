34a0cafb540f9eaff42161f02125c59d54d892bb
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-06-03 14:01:36 +0200

    Added alltime earnings and expenses to user

--- models/user.go
+++ models/user.go
@@ -10,13 +10,16 @@ const (
 // User is a user.
 type User struct {
 	gorm.Model
-	GoogleID          string  `json:"-"`
-	Fences            []Fence `json:"fences"`
-	PrivateKey        string  `sql:"size:4096" json:"-"`
-	GCMNotificationID string  `json:"-"`
-	Name              string  `json:"name"`
-	AvatarURL         string  `json:"avatar_url"`
-	CoverURL          string  `json:"cover_url"`
-	Balance           float64 `json:"balance"`
-	LastKnownGeoHash  string  `json:"-"`
+	GoogleID                string  `json:"-"`
+	Fences                  []Fence `json:"fences"`
+	PrivateKey              string  `sql:"size:4096" json:"-"`
+	GCMNotificationID       string  `json:"-"`
+	Name                    string  `json:"name"`
+	AvatarURL               string  `json:"avatar_url"`
+	CoverURL                string  `json:"cover_url"`
+	Balance                 float64 `json:"balance"`
+	LastKnownGeoHash        string  `json:"-"`
+	EarningsRentAllTime     float64 `json:"-"`
+	ExpensesRentAllTime     float64 `json:"-"`
+	ExpensesGeoFenceAllTime float64 `json:"-"`
 }
