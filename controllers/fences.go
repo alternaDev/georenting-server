2606a95a83b65a444f590057f133a422c78130ab
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-11-06 15:24:29 +0100

    Fix new parameter type

--- controllers/fences.go
+++ controllers/fences.go
@@ -495,7 +495,7 @@ func RemoveFenceHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = search.DeleteGeoFence(fence)
+	err = search.DeleteGeoFence(int(fence.ID))
 
 	if err != nil {
 		log.Printf("Error while deleting Fence from Search: %s", err.Error())
